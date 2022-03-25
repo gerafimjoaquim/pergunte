a50546141a1a9314d9d6740b3b77d3de8fea4885
Gerafim Joaquim <oi@gerafim.com>
2022-03-25 12:21:55 +0200

    Actualizado

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const connection = require('./database/database')
 const app = express()
+
+
+connection.authenticate()
+    .then(() => {
+        console.log('Autenticado')
+    })
+    .catch(error => console.log(`Erro na autenticação: ${error}`))
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -10,6 +21,13 @@ app.get('/', (req, res) => {
 
 app.get('/perguntar', (req, res) => {
     res.render('perguntar')
+})
+
+app.post('/gravarpergunta', (req, res) => {
+    const titulo = req.body.titulo
+    const descricao = req.body.descricao
+
+    res.send(`<li>${titulo}</li><li>${descricao}</li>`)
 })
 
 app.listen(8080, () => {console.log('App rodando...')})
